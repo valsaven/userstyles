d23aae631a9177d1d6fa52a672338db15ce8da00
Val Saven <val.saven@gmail.com>
2018-02-27 14:46:05 +0300

    Restyle gulpfile with Prettier

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,14 @@ var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var del = require('del');
 
-gulp.task('default', function () {
-    del(['dist/**', '!dist']).then(paths => {
-        console.log('Deleted files and folders:\n', paths.join('\n'));
-    });
+gulp.task('default', function() {
+  del(['dist/**', '!dist']).then(paths => {
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+  });
 
-    return gulp.src([
-            '!node_modules/**/*.scss',
-            './**/*.scss',
-        ])
-        .pipe(sass())
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('dist'))
+  return gulp
+    .src(['!node_modules/**/*.scss', './**/*.scss'])
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'));
 });
