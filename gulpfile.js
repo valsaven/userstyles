1874d021727d2260c16b5336dc6c04e6c5ae4805
Val Saven <val.saven@gmail.com>
2019-02-19 11:40:25 +0300

    Move to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,20 @@ const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const del = require('del');
 
+sass.compiler = require('node-sass');
+
 gulp.task('default', () => {
   del(['dist/**', '!dist']).then(paths => {
     console.log('Deleted files and folders:\n', paths.join('\n'));
   });
 
   return gulp
-    .src(['!node_modules/**/*.scss', './**/*.scss'])
-    .pipe(sass())
+    .src(['scss/**/*.scss'])
+    .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', () => {
-  gulp.watch('./**/*.scss', ['default']);
+  gulp.watch('./**/*.scss', gulp.parallel('default'));
 });
